da0656d0ba6c0fa2d3434fc5dedb63f37f395526
Omar-Nabil2 <omarabosalem66@gmail.com>
2026-08-10 16:56:24 +0300

    category entity

---
+++ src/EShop.Console/Entities/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace EShop.Console.Entities;
+
+public class Category
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int? ParentCategoryId { get; set; }
+    public List<Category> subcategories { get; set; }
+}
