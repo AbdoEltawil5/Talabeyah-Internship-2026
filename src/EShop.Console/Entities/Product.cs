a7bbdadf4ad355d016b3a47615a6711981808e00
basem <basem.kasem16@gmail.com>
2026-08-10 17:17:54 +0300

    create product entity

---
+++ src/EShop.Console/Entities/Product.cs
@@ -0,0 +1,11 @@
+namespace EShop.Console.Entities;
+
+public class Product
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = null!;
+    public string Description { get; set; } = null!;
+    public decimal Price { get; set; }
+    public int StockQuantity { get; set; }
+    public Guid CategoryId { get; set; }
+}
