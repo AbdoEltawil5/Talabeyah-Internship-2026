61ba859c0eae50a2ccc8a7842c9fb34e74dfe2a4
basem <basem.kasem16@gmail.com>
2026-08-10 19:15:59 +0300

    update cart class to apply proper oop prenciples. rename UserId to CustomerId for naming consistancy

--- src/EShop.Console/Entities/Cart.cs
+++ src/EShop.Console/Entities/Cart.cs
@@ -2,8 +2,13 @@ namespace EShop.Console.Entities;
 
 public class Cart
 {
-    
-    public Guid Id { get; set; }
-    public Guid UserId { get; set; }
-    public List<CartItem> Items { get; set; }
+    public Cart(Guid customerId, List<CartItem>? items)
+    {
+        Id = Guid.NewGuid();
+        CustomerId = customerId;
+        Items = items ?? new List<CartItem>();
+    }
+    public Guid Id { get; }
+    public Guid CustomerId { get; }
+    public List<CartItem> Items { get; }
 }
