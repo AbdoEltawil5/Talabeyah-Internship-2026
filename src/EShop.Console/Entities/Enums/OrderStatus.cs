a7dab0fd654301e4612806100ff038e23f77f8e5
basem <basem.kasem16@gmail.com>
2026-08-11 16:36:13 +0300

    update status to be OrderStatus for specifity.

--- src/EShop.Console/Entities/Enums/OrderStatus.cs
+++ src/EShop.Console/Entities/Enums/OrderStatus.cs
@@ -1,6 +1,6 @@
 namespace EShop.Console.Entities.Enums;
 
-public enum Status
+public enum OrderStatus
 {
     Pending,
     Approved,
--- src/EShop.Console/Entities/Order.cs
+++ src/EShop.Console/Entities/Order.cs
@@ -5,11 +5,11 @@ namespace EShop.Console.Entities;
 
 public class Order : ISummarizable
 {
-    public Order(Guid id, Guid customerId, List<OrderItem>? orderItems, Status status = Status.Pending)
+    public Order(Guid id, Guid customerId, List<OrderItem>? orderItems, OrderStatus orderStatus = OrderStatus.Pending)
     {
         Id = id;
         CustomerId = customerId;
-        Status = status;
+        OrderStatus = orderStatus;
         CreatedAt =  DateTime.UtcNow;
         OrderItems = orderItems ?? new List<OrderItem>();
         TotalAmount = GetTotalAmount();
@@ -17,7 +17,7 @@ public class Order : ISummarizable
 
     public Guid Id { get; set; }
     public Guid CustomerId { get; }
-    public Status Status { get; set; }
+    public OrderStatus OrderStatus { get; }
     public decimal TotalAmount { get; }
     public DateTime CreatedAt { get; }
     public List<OrderItem> OrderItems { get; }
@@ -33,7 +33,7 @@ public class Order : ISummarizable
                 ----------------------
                 Order #{Id} 
                 Created at: {CreatedAt}
-                Current Status: {Status}
+                Current Status: {OrderStatus}
                 ======================
                 TotalAmount: {TotalAmount}
                 ----------------------
