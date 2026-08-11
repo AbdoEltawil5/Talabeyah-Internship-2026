2cca474b28de6b703eb9002e036e6ba2b5f2b26a
basem <basem.kasem16@gmail.com>
2026-08-11 13:40:30 +0300

    Create an order Processor to take new orders from a user

--- src/EShop.Console/Program.cs
+++ src/EShop.Console/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using EShop.Console;
+using EShop.Console.Repositories.Implementations;
+using EShop.Console.Services.Implementations;
+
+var orderRepository = new OrderRepository();
+var productRepository = new ProductRepository();
+var orderService = new OrderService(orderRepository);
+var productService = new ProductService(productRepository);
+
+OrderProcessor orderProcessor = new OrderProcessor(orderService, productService);
+
+orderProcessor.AddOrder();
