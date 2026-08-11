e909a61fcfe8f987352eaa225ded67932ee3e019
basem <basem.kasem16@gmail.com>
2026-08-11 13:33:36 +0300

    add order repository to add orders in memory, and get an in-memory list of dummy orders

---
+++ src/EShop.Console/Repositories/Implementations/OrderRepository.cs
@@ -0,0 +1,41 @@
+using EShop.Console.Entities;
+using EShop.Console.Entities.Enums;
+using EShop.Console.Repositories.Interfaces;
+
+namespace EShop.Console.Repositories.Implementations;
+
+public class OrderRepository : IOrderRepository
+{
+    private List<Order> _orders;
+
+    public OrderRepository()
+    {
+        _orders = new List<Order>();
+
+        for (int i = 1; i <= 5; i++)
+        {
+            var order = new Order(Guid.NewGuid(), Guid.NewGuid(), null);
+            var random = new Random();
+            for (int j = 0; j < 3; j++)
+            {
+                var unitPrice = random.Next(10, 100);
+                var discount = random.Next(1, unitPrice / 2);
+                var item = new OrderItem(order.Id, Guid.NewGuid(), unitPrice, discount);
+                order.OrderItems.Add(item);
+            }
+
+            _orders.Add(order);
+        }
+    }
+
+    public IEnumerable<Order> GetAll()
+    {
+        return _orders;
+    }
+
+    public Guid Add(Order order)
+    {
+        _orders.Add(order);
+        return order.Id;
+    }
+}
