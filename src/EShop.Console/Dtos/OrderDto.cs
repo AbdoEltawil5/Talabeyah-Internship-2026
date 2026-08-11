2b59c834fa86f96430b74716e43921c6a9f30b2d
basem <basem.kasem16@gmail.com>
2026-08-11 16:43:18 +0300

    change dtos from class to record for immutability

--- src/EShop.Console/Dtos/OrderDto.cs
+++ src/EShop.Console/Dtos/OrderDto.cs
@@ -3,16 +3,5 @@ using EShop.Console.Entities.Enums;
 
 namespace EShop.Console.Dtos;
 
-public class OrderDto
-{
-    public OrderDto(Guid customerId, List<OrderItemDto> orderItemsDto, Status status = Entities.Enums.Status.Pending)
-    {
-        CustomerId = customerId;
-        Status = status;
-        OrderItemsDto = orderItemsDto;
-    }
-    public Guid CustomerId  { get; set; }
-    public Status? Status { get; set; }
-    public List<OrderItemDto> OrderItemsDto { get; set; }
-        
-}
+public record OrderDto(Guid CustomerId, List<OrderItemDto> OrderItemsDto, OrderStatus OrderStatus = OrderStatus.Pending);
+
--- src/EShop.Console/Dtos/OrderItemDto.cs
+++ src/EShop.Console/Dtos/OrderItemDto.cs
@@ -1,15 +1,3 @@
 namespace EShop.Console.Dtos;
 
-public class OrderItemDto
-{
-    public OrderItemDto(Guid productId, decimal unitPrice, decimal discount)
-    {
-        productId = ProductId;
-        UnitPrice = unitPrice;
-        Discount = discount;
-    }
-    
-    public Guid ProductId { get; set; }
-    public decimal UnitPrice { get; set; }
-    public decimal Discount { get; set; }
-}
+public record OrderItemDto(Guid ProductId, decimal UnitPrice, decimal Discount);
