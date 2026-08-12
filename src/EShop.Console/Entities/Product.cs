2d28c1fed8a35bc1ed43bf37c682f995e907d158
basem <basem.kasem16@gmail.com>
2026-08-12 14:53:39 +0300

    update all money datatype from "decimal" to be "Money"

--- src/EShop.Console/Entities/Product.cs
+++ src/EShop.Console/Entities/Product.cs
@@ -1,4 +1,5 @@
 using EShop.Console.Abstractions;
+using EShop.Console.Shared;
 
 namespace EShop.Console.Entities;
 
@@ -7,12 +8,12 @@ public class Product : ISummarizable
     public Guid Id { get; private set; }
     public string Name { get; private set; }
     public string Description { get; private set; }
-    public decimal Price { get; private set; }
+    public Money Price { get; private set; }
     public int StockQuantity { get; private set; }
     public Guid CategoryId { get; private set; }
     public Category Category { get; private set; }
 
-    public Product(Guid id, string name, string description, decimal price, int stockQuantity, Category category)
+    public Product(Guid id, string name, string description, Money price, int stockQuantity, Category category)
     {
         if (id == Guid.Empty)
             throw new ArgumentException("Id cannot be empty");
