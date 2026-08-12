a5999d27c5a65e856037b9f0e668a69498d559b8
philosopher <200029096@must.edu.eg>
2026-08-12 17:06:02 +0300

    Demo for deferred execution in Program.cs

--- src/EShop.Console/Program.cs
+++ src/EShop.Console/Program.cs
@@ -8,15 +8,34 @@ var parentCategory = new Category(Guid.NewGuid(), "Electronics", null);
 var phones = new Category(Guid.NewGuid(), "Phones", parentCategory.Id);
 parentCategory.AddSubCategory(phones);
 
-var product = new Product(Guid.NewGuid(), "Iphone", "Iphone 17 pro max", 
-    new Money(50000m), 10, phones);
-phones.AddProduct(product);
+var product1 = new Product(Guid.NewGuid(), "Iphone", "Iphone 17 pro max", new Money(50000m), 10, phones);
+var product2 = new Product(Guid.NewGuid(), "Samsung", "Samsung S25", new Money(40000m), 8, phones);
+var product3 = new Product(Guid.NewGuid(), "Pixel", "Pixel 9", new Money(35000m), 5, phones);
+phones.AddProduct(product1);
+phones.AddProduct(product2);
+phones.AddProduct(product3);
 
 var customer = new Customer(Guid.NewGuid(), "Baselyosry", "baselyosry@gmail.com", "password");
 var cart = new Cart(Guid.NewGuid(), customer.Id);
-cart.AddItem(product.Id, 1);
+cart.AddItem(product1.Id, 1);
+cart.AddItem(product2.Id, 1);
+cart.AddItem(product3.Id, 1);
 
-var products = new List<Product>(){product};
+var products = new List<Product> { product1, product2, product3 };
+
+var productService = new ProductService(products);
+
+Console.WriteLine("Calling GetProducts");
+var result = productService.GetProducts();
+Console.WriteLine("GetProducts returned (iterator should not have run yet)");
+
+Console.WriteLine("Starting foreach");
+foreach (var p in result)
+{
+    Console.WriteLine($"{p.Name}");
+}
+Console.WriteLine("Foreach done");
+Console.WriteLine();
 
 IStockValidator stockValidator = new StockValidator();
 IDiscountService discountService = new PercentageDiscount(10);
@@ -29,7 +48,10 @@ var orderProcessor = new OrderProcessor(
 
 var order = orderProcessor.PlaceOrder(cart, products);
 
-Console.WriteLine(product.Summarize());
+Console.WriteLine(product1.Summarize());
+Console.WriteLine(product2.Summarize());
+Console.WriteLine(product3.Summarize());
+
 Console.WriteLine(cart.Summarize());
 Console.WriteLine(order.Summarize());
 Console.WriteLine(customer.Summarize());
