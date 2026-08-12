12b58ae42843f453abe2defe1e005ba61cd84f3c
Omar-Nabil2 <omarabosalem66@gmail.com>
2026-08-12 17:36:45 +0300

    remove test case.

--- src/EShop.Console/Entities/Product.cs
+++ src/EShop.Console/Entities/Product.cs
@@ -7,7 +7,7 @@ public class Product : ISummarizable
     public Guid Id { get; private set; }
     public string Name { get; private set; }
     public string Description { get; private set; }
-    public Money Price { get;  set; }
+    public Money Price { get; private set; }
     public int StockQuantity { get; private set; }
     public Guid CategoryId { get; private set; }
     public Category Category { get; private set; }
