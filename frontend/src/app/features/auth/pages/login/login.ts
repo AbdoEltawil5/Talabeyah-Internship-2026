1ba0085273da1d046b1f6225c41e8f07389f6770
philosopher <200029096@must.edu.eg>
2026-08-20 13:01:59 +0300

    Implement login functionality with form validation and error handling

--- frontend/src/app/features/auth/pages/login/login.ts
+++ frontend/src/app/features/auth/pages/login/login.ts
@@ -1,12 +1,58 @@
-import { Component, inject } from '@angular/core';
-import { AuthService } from '../../services';
+import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './login.html',
-  styleUrl: './login.scss',
+  styleUrls: ['./login.scss']
 })
 export class Login {
-  private authService = inject(AuthService);
+
+  email: string = '';
+  password: string = '';
+  loading: boolean = false;
+  hasError = signal(false);
+  errorMessage: string = '';
+  successMessage: string = '';
+
+  constructor(private authService: AuthService) { }
+
+  onSubmit() {
+    if (this.email == '' || this.password == '') {
+      this.errorMessage = 'Please enter email and password';
+      this.hasError.set(true);
+      this.successMessage = '';
+      return;
+    }
+
+
+    this.errorMessage = '';
+    this.hasError.set(false);
+    this.successMessage = '';
+    this.loading = true;
+
+    this.authService.login(this.email, this.password).subscribe({
+      next: (response: any) => {
+        // TODO:- to be implemented in an interceptor 
+        this.authService.saveToken(response.Token);
+        this.loading = false;
+        this.successMessage = 'Login successful';
+      },
+      error: (error: any) => {
+        this.loading = false;
+        this.hasError.set(true);
+        this.successMessage = '';
+        if (error.status == 401) {
+          this.errorMessage = 'Invalid email or password';
+        } else {
+          this.errorMessage = 'Something went wrong, please try again';
+        }
+      }
+    })
+
+  }
+
 }
